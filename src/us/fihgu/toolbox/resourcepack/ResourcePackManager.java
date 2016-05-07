0379162969cc4e79f4091b08727b51b1fd1e443f
fihgu <fihgus@hotmail.com>
2016-05-07 15:39:36 -0500

    t

--- src/us/fihgu/toolbox/resourcepack/ResourcePackManager.java
+++ src/us/fihgu/toolbox/resourcepack/ResourcePackManager.java
@@ -5,6 +5,12 @@ import java.io.OutputStream;
 import java.io.IOException;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
+import java.io.File;
+import us.fihgu.toolbox.json.JsonUtils;
+import us.fihgu.toolbox.file.FileUtils;
+import java.util.LinkedList;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class ResourcePackManager
 {
@@ -15,31 +21,95 @@ public class ResourcePackManager
 	*/
 	private static HashMap<String, String> resourceUsers = new HashMap<String, String>();
 	
+	private static HashMap<String, String> oldResourceUsers;
+	
+	private static File saveFile = new File("./fihgu/toolbox/resource/");
+	
+	private static LinkedList<InputStream> resources = new LinkedList<>();
+	
 	/**
-	* custom items based on diamond hoes
+	* register a resource pack to be combined into server resource pack.<br>
+	* you may only use this method inside your onEnabl() method, else the resource will not be registered correctly.<br>
+	* when a new resource being registered the first time, the server chache will be reconstructed.<br>
 	*/
-	private static HashMap<String, String> customItems = new HashMap<String, String>();
-	
-	public static void registerResource(JavaPlugin plugin, OutputStream source) throws IOException
+	public static void registerResource(JavaPlugin plugin, InputStream source) throws IOException
 	{
-	
+		resourceUsers.put(plugin.getName() ,plugin.getDescription().getVersion());
+		resources.add(source);
 	}
 	
 	public static void Load()
 	{
-		
+		oldResourceUsers = (HashMap<String, String>) JsonUtils.fromFile(saveFile, HashMap.class);
 	}
 	
 	public static void save()
 	{
-		
+		JsonUtils.toFile(saveFile, resourceUsers);
 	}
 	
-	public static void buildResourcePack()
+	private static boolean needsRebuild()
 	{
-		//TODO
+		if(oldResourceUsers == null)
+		{
+			if(resourceUsers.size() > 0)
+			{
+				return true;
+			}
+		}
+		else
+		{
+			if(resourceUsers.size() != oldResourceUsers.size())
+			{
+				return true;
+			}
+			
+			for(String key : resourceUsers.keySet())
+			{
+				String value = resourceUsers.get(key);
+				String oldValue = oldResourceUsers.get(key);
+				
+				if(!value.equals(oldValue))
+				{
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 	
+	public static void buildResourcePack() throws IOException
+	{
+		if(needsRebuild())
+		{
+			File work = new File("./fihgu/toolbox/resource/work/");
+			FileUtils.deleteFolder(work);
+			FileUtils.createFileAndPath(work);
+			File temp = new File("./fihgus/toolbox/resource/work/temp.zip");
+			FileUtils.createFileAndPath(temp);
+			for(InputStream in : resources)
+			{
+				FileOutputStream fileOut = new FileOutputStream(temp);
+				byte[] buffer = new byte[4096];
+				int byteRead = 0;
+				while((byteRead = in.read(buffer)) > 0)
+				{
+					fileOut.write(buffer, 0, byteRead);
+				}
+				fileOut.close();
+			}
+		}
+		
+		for(InputStream in: resources)
+		{
+			in.close();
+		}
+	}
+	
+	/**
+	* force player
+	*/
 	public static void setForceResourcePack()
 	{
 		ResourcePackManager.force = true;
